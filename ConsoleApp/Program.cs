173a8bed3c345dbecdd0687c130e09d554037f69
JCorrea13 <josemanuelcorrea.13@gmail.com>
2020-05-04 22:26:25 -0500

    project init

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using EfCore.Data;
+using EFCore.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        private static SamuraiContext context = new SamuraiContext();
+        static async Task Main(string[] args)
+        {
+            context.Database.EnsureCreated();
+            await GetSamurais("Before Add:");
+            await AddSamurai();
+            await GetSamurais("After Add:");
+            Console.WriteLine("Press any key...");
+            Console.ReadKey();
+        }
+
+        private static async Task GetSamurais(string text)
+        {
+            var samurais = await context.Samurais.ToListAsync();
+            Console.WriteLine($"{text}: Samurai count is {samurais.Count}");
+            foreach (var samurai in samurais)
+            {
+                Console.WriteLine(samurai.Name);
+            }
+        }
+
+        private static async Task AddSamurai()
+        {
+            var samurai = new Samurai { Name = "Sampson" };
+            context.Samurais.Add(samurai);
+            await context.SaveChangesAsync();
+        }
+    }
+}
