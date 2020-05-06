a587015f28e87d95c85e6a2a79a341cc357e461e
JCorrea13 <josemanuelcorrea.13@gmail.com>
2020-05-05 21:52:20 -0500

    removign all

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -8,31 +8,8 @@ namespace ConsoleApp
 {
     internal class Program
     {
-        private static SamuraiContext context = new SamuraiContext();
         static async Task Main(string[] args)
         {
-            await GetSamurais("Before Add:");
-            await AddSamurai();
-            await GetSamurais("After Add:");
-            Console.WriteLine("Press any key...");
-            Console.ReadKey();
-        }
-
-        private static async Task GetSamurais(string text)
-        {
-            var samurais = await context.Samurais.ToListAsync();
-            Console.WriteLine($"{text}: Samurai count is {samurais.Count}");
-            foreach (var samurai in samurais)
-            {
-                Console.WriteLine(samurai.Name);
-            }
-        }
-
-        private static async Task AddSamurai()
-        {
-            var samurai = new Samurai { Name = "Sampson" };
-            context.Samurais.Add(samurai);
-            await context.SaveChangesAsync();
         }
     }
 }
