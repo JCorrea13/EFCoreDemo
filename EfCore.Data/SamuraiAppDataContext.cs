bca65bc09074ebf1b7614ece38098a9e05ec582f
JCorrea13 <josemanuelcorrea.13@gmail.com>
2020-05-06 21:40:27 -0500

    adding a logger to EFCore

--- EfCore.Data/SamuraiAppDataContext.cs
+++ EfCore.Data/SamuraiAppDataContext.cs
@@ -1,6 +1,6 @@
-﻿using EfCore.Model;
-using EFCore.Model;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using EfCore.Model;
 
 namespace EfCore.Data
 {
@@ -12,11 +12,22 @@ namespace EfCore.Data
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Battle> Battles { get; set; }
 
+        public static readonly ILoggerFactory ConsoleLoggerFactory
+            = LoggerFactory.Create(builder =>
+            {
+                builder.AddFilter((category, level) =>
+                    category == DbLoggerCategory.Database.Command.Name
+                    && level == LogLevel.Information)
+                .AddConsole();
+            });
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = SamuraiAppData");
+                optionsBuilder
+                    .UseLoggerFactory(ConsoleLoggerFactory)
+                    .UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = SamuraiAppData");
             }
         }
 
