636b50ee90b9aa3ef5ce5d3cb7278d281e11b78c
JCorrea13 <josemanuelcorrea.13@gmail.com>
2020-05-06 22:52:11 -0500

    Adding notTracking context for disconnected scenarios

---
+++ EfCore.Data/SamuraiAppDataContextNoTraking.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using EfCore.Model;
+using EFCore.Model;
+
+namespace EfCore.Data
+{
+    public partial class SamuraiAppDataContextNoTraking : DbContext
+    {
+        public SamuraiAppDataContextNoTraking()
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        }
+
+        public DbSet<Clan> Clans { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+
+        public static readonly ILoggerFactory ConsoleLoggerFactory
+            = LoggerFactory.Create(builder =>
+            {
+                builder.AddFilter((category, level) =>
+                    category == DbLoggerCategory.Database.Command.Name
+                    && level == LogLevel.Information)
+                .AddConsole();
+            });
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseLoggerFactory(ConsoleLoggerFactory)
+                    .EnableSensitiveDataLogging()
+                    .UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = SamuraiAppData");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SamuraiBattle>().HasKey(x => new { x.SamuraiId, x.BattleId });
+            modelBuilder.Entity<Horse>().ToTable("Horses");
+        }
+
+    }
+}
