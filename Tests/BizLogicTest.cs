abdd35be49764d165ec021c427669f1b55503bb4
JCorrea13 <josemanuelcorrea.13@gmail.com>
2020-05-10 00:16:50 -0500

    usign Result method instead of GetAwaiter().GetResult()

--- Tests/BizLogicTest.cs
+++ Tests/BizLogicTest.cs
@@ -19,7 +19,7 @@ namespace Tests
             var bizLogic = new BusinessLogic(context);
             var nameList = new string[] { "1", "2", "3" };
 
-            var result = bizLogic.AddMultipleSamurais(nameList).GetAwaiter().GetResult();
+            var result = bizLogic.AddMultipleSamurais(nameList).Result;
             Assert.AreEqual(nameList.Length, result);
         }
 
@@ -31,7 +31,7 @@ namespace Tests
             using (var context = new SamuraiTestDataContext(builder.Options))
             {
                 var bizLogic = new BusinessLogic(context);
-                bizLogic.InsertNewSamurai(new Samurai());
+                _ = bizLogic.InsertNewSamurai(new Samurai()).Result;
             }
 
             using var context2 = new SamuraiTestDataContext(builder.Options);
