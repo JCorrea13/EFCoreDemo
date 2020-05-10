bdd42db157195ef75d0dd716ffba610d821f6c11
JCorrea13 <josemanuelcorrea.13@gmail.com>
2020-05-09 19:07:43 -0500

    adding some procedures via a migration

---
+++ EfCore.Data/Migrations/20200509235640_Adding-StoreProcedures.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EfCore.Data.Migrations
+{
+    public partial class AddingStoreProcedures : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                @"CREATE PROCEDURE dbo.SamuraisWhoSaidAWord
+	                @text varchar(20)
+	                AS
+	                SELECT Samurais.Id, Samurais.Name, Samurais.ClanId
+	                FROM Samurais
+	                INNER JOIN Quotes ON Samurais.Id = Quotes.SamuraiId
+	                WHERE (Quotes.Text LIKE '%'+@text+'%')");
+            migrationBuilder.Sql(
+                @"CREATE PROCEDURE dbo.DeleteQuotesForSamurai
+	                @samuraiId int
+	                AS
+	                DELETE FROM Quotes
+	                WHERE Quotes.SamuraiId = @samuraiId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP PROCEDURE dbo.SamuraisWhoSaidAWord");
+            migrationBuilder.Sql("DROP PROCEDURE dbo.DeleteQuotesForSamurai");
+        }
+    }
+}
