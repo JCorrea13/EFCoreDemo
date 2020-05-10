23bd51a9e22b0210be11474c25b36066586b4004
JCorrea13 <josemanuelcorrea.13@gmail.com>
2020-05-09 20:52:59 -0500

    modifying the asp.net exaple

--- SamuraiAPI/Controllers/SamuraisController.cs
+++ SamuraiAPI/Controllers/SamuraisController.cs
@@ -25,7 +25,7 @@ namespace SamuraiAPI.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Samurai>>> GetSamurais()
         {
-            return await _context.Samurais.ToListAsync();
+            return await _context.Samurais.Include(x => x.Horse).ToListAsync();
         }
 
         // GET: api/Samurais/5
